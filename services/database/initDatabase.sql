05e4001afe7f5a8fb9d0c97be50d6e79e00b99c0
Brandon Phan <brandon.phan.73@gmail.com>
2019-03-29 10:42:38 -0700

    Add user database

---
+++ services/database/initDatabase.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS Users; 
+CREATE TABLE Users (
+    id 							VARCHAR(36) NOT NULL, 
+    user_role 				    CHAR(5) NOT NULL,
+    username                    VARCHAR(64) NOT NULL,
+    password_hash               VARCHAR(64) NOT NULL,
+    
+    primary key (id)
+);
+
+set @UserId1 = UUID();
+set @UserId2 = UUID();
+
+INSERT INTO Users(id, user_role, username, password_hash) VALUES (@UserId1, 'USER', 'bphun', '1234');
+INSERT INTO Users(id, user_role, username, password_hash) VALUES (@UserId2, 'ADMIN', 'admin', '1234');
+
+SELECT * FROM Users;
+
+use mysql;
+GRANT ALL ON *.* to root@'172.17.0.1' IDENTIFIED BY '1234';
+FLUSH PRIVILEGES;
