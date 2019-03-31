00c272eff739fd4f71d5eab2dc5c979b4ce3e97d
Brandon Phan <brandon.phan.73@gmail.com>
2019-03-30 22:50:37 -0700

    Added Dockerfiles for each service and docker-compose.yml for entire project to make it easier to start the services

--- services/movie-info-service/src/main/java/com/bphan/movieinfoservice/MovieResource.java
+++ services/movie-info-service/src/main/java/com/bphan/movieinfoservice/MovieResource.java
@@ -1,13 +1,11 @@
 package com.bphan.movieinfoservice;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-@EnableEurekaClient
 public class MovieResource {
 
     @RequestMapping("/{movieId}")
