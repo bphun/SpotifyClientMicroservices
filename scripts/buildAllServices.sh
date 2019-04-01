3f418096ca3a08987c60a495985da6bfe3327cdc
Brandon Phan <brandon.phan.73@gmail.com>
2019-04-01 14:57:15 -0700

    Add bash script that builds all services

---
+++ scripts/buildAllServices.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+for service in $(ls services); 
+do
+
+    if [ "$service" = "database" ]; then
+        continue
+    fi
+
+    echo Building $service...
+
+    cd services/$service
+    gradle build -x test
+
+    if [ $? -eq 0 ]; then
+        printf "${GREEN}Successfully built $service${NC}\n"
+    else
+        printf "${RED}Build of $service failed${NC}\n"
+    fi
+
+    cd ../../
+
+done
